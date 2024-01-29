3aa841f2bd3e75161560db368e7da1d5129d36e1
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-01-29 18:51:12 -0500

    smokes prefab completed, i need to watch so many videos to understand opacity ;-;

--- Assets/Assignment/script/Smoke.cs
+++ Assets/Assignment/script/Smoke.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-//
+
 public class Smoke : MonoBehaviour
 {
+    //give it spriterenerer and definetion of when to change opacity
+    private SpriteRenderer smokeRenderer;
+    private float opacityChangeTimer = 0f;
+    private float opacityChangeInterval = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        smokeRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        opacityChangeTimer += Time.deltaTime;
+
+        //check if the interval for opacity change has elapsed
+        if (opacityChangeTimer >= opacityChangeInterval )
+        {
+            //reset the timer
+            opacityChangeTimer = 0f; 
+
+            if (smokeRenderer.color.a ==1.0f)
+            {
+                smokeRenderer.color = new Color(smokeRenderer.color.r, smokeRenderer.color.g, smokeRenderer.color.b, 0.5f);
+            }
+            else
+            {
+                smokeRenderer.color = new Color(smokeRenderer.color.r, smokeRenderer.color.g, smokeRenderer.color.b, 0.0f);
+            }
+        }
     }
 }
