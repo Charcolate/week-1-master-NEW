996c8b42b429ba1da213662d555e31e32093d562
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-01-30 14:31:48 -0500

    the steak is moving fine but the butter opacity does not seems to be changing,.

--- Assets/Assignment/script/SteakMove.cs
+++ Assets/Assignment/script/SteakMove.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class SteakMove : MonoBehaviour
 {
+    //give the steak basic states for moving around
+    Vector2 direction;
+    public float maxSpeed = 500;
+    Rigidbody2D rb;
+    public float forwardSpeed = 500;
+    public float steeringSpeed = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(direction * forwardSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Whoops! Almost fell out of pan!");
     }
 }
  
